75cf63c59f841381afc209c3bd8f3c5b5a1f83d0
jihun.kim <jihun.kim@testbank.ai>
2023-09-01 11:08:41 +0900

    110

--- example/data/mutations/deleteQuiz.ts
+++
@@ -1,4 +0,0 @@
-import { gql } from '@apollo/client';
-export const DELETE_QUIZ = gql`mutation deleteQuiz($DeleteQuizRequestSchema: DeleteQuizRequestSchema!){
-    deleteQuiz(DeleteQuizRequestSchema: $DeleteQuizRequestSchema)
-}`
--- example/data/mutations/deleteQuizPaper.ts
+++
@@ -1,4 +0,0 @@
-import { gql } from '@apollo/client';
-export const DELETE_QUIZ_PAPER = gql`mutation deleteQuizPaper($DeleteQuizPaperRequestSchema: DeleteQuizPaperRequestSchema!){
-    deleteQuizPaper(DeleteQuizPaperRequestSchema: $DeleteQuizPaperRequestSchema)
-}`
